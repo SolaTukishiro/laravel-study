ee329081ae9d7a0e8c6749b2e48c1e513211084d
TakumaAndo MBP <antaku0514@icloud.com>
2025-03-02 18:58:22 +0900

    First Commit

---
+++ resources/views/helloUser.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>ようこそ{{$name}}さん</p>
+    <p>本コースでは{{ $source }}を学んでいきます。</p>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+use App\Http\Controllers\UtilityController;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\RequestSampleController;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+Route::get('/hello-world', fn() =>
+    view('hello')
+);
+
+Route::get('/hello', fn() =>
+    view('helloUser',[
+        'name' => '山田',
+        'source' => 'Laravel',
+    ])
+);
+
+Route::get('/',fn() => view('index'));
+Route::get('/curriculum',fn() => view('curriculum'));
+
+// 世界の時間
+Route::get('/world-time', [UtilityController::class, 'worldTime']);
+
+// おみくじ
+Route::get('/omikuji', [GameController::class, 'omikuji']);
+
+// モンティ・ホール問題
+Route::get('/monty-hall',[GameController::class, 'montyFall']);
+
+Route::get('/form', [RequestSampleController::class, 'form']);
+
+Route::get('query-strings', [RequestSampleController::class, 'queryStrings']);
+
+Route::get('/user/{id}', [RequestSampleController::class, 'profile'])->name(name:'profile');
+
+Route::get('/products/{category}/{year}', [RequestSampleController::class, 'productsArchive']);
+
+Route::get('/route-link', [RequestSampleController::class, 'routeLink']);
