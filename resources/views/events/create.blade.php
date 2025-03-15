03ca5a7b06c783cbf719cf2d77c8279996b386d8
TakumaAndo MBP <antaku0514@icloud.com>
2025-03-15 17:50:38 +0900

    セクション8 done

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -2,9 +2,12 @@
 
 @section('title', 'イベント登録')
 @section('content')
-<form action="{{ route('events.store') }}" method=post>
-    @csrf
-    <lavel>イベント名: <input type="text" name="title"></lavel>
-    <button type="submit">登録</button>
-</form>
+    @if(session()->has('success'))
+        <p>{{ session()->get( 'success' )}}</p>
+    @endif
+    <form action="{{ route('events.store') }}" method=post>
+        @csrf
+        <lavel>イベント名: <input type="text" name="title"></lavel>
+        <button type="submit">登録</button>
+    </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\UtilityController;
 use App\Http\Controllers\GameController;
+use App\Http\Controllers\HiLowController;
 use App\Http\Controllers\RequestSampleController;
 use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Route;
@@ -45,3 +46,7 @@ Route::get('/login',[RequestSampleController::class, 'loginForm']);
 Route::post('/login', [RequestSampleController::class, 'login'])->name('login');
 
 Route::resource('/events', EventController::class)->only(['index', 'create', 'store']);
+
+// ハイローゲーム
+Route::get('/hi-low', [HiLowController::class, 'index'])->name('hi-low');
+Route::post('/hi-low', [HiLowController::class, 'result']);
